eceb213e497ad206383784af532bc65c98af6bc8
Dima <soloveid87@mail.ru>
2022-08-06 18:48:23 +1000

    jsonobject and movies from it

--- app/src/main/java/com/dima/mymovies/MainActivity.java
+++ app/src/main/java/com/dima/mymovies/MainActivity.java
@@ -3,6 +3,16 @@ package com.dima.mymovies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.dima.mymovies.data.Movie;
+import com.dima.mymovies.utils.JSONUtils;
+import com.dima.mymovies.utils.NetworkUtils;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(NetworkUtils.POPULARITY,5);
+        ArrayList<Movie> movies = JSONUtils.getMoviesFromJSON(jsonObject);
+        StringBuilder builder = new StringBuilder();
+        for(Movie movie:movies){
+            builder.append(movie.getTitle()).append("\n");
+        }
+        Log.i("MyResult",builder.toString());
     }
 }
